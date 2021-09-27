d9823730d60b7ea5e029be00df59b72922b38ecb
Andrew Peck <andrew.peck@cern.ch>
2021-09-27 16:25:44 -0400

    cleanup script

--- billing/sum_tables.py
+++ billing/sum_tables.py
@@ -1,32 +1,34 @@
 import csv
 import datetime
-import pprint
 import re
 import sys
 import os
-import re
 from collections import OrderedDict
-from glob import glob
-
+#import pprint
+#from glob import glob
 
 ONEDAY = datetime.timedelta(days=1)
 
-def get_csv_files ():
+
+def get_csv_files():
+    "Get a list of CSV files matching the date format e.g. 2021-09.csv"
     res = [f for f in os.listdir("./") if re.search(r'.[0-9]+-[0-9]+\.csv$', f)]
     res.sort()
     return res
 
-def princ (string):
+
+def princ(string):
     """Wrapper to print without a newine... uhg"""
-    print (string, end='')
+    print(string, end='')
 
-def substitute_project_name (name):
 
-    def subst (name, foo, bar):
-        if name==foo:
-            return bar
-        else:
-            return name
+def substitute_project_name(name):
+    "Replace some alternative project names with the canconical names"
+
+    def subst(name, vara, varb):
+        if name == vara:
+            return varb
+        return name
 
     """Common substitions from shorthand to EDF database names"""
     name = subst(name, "ETL", "CMS-ETL")
@@ -44,8 +46,10 @@ def substitute_project_name (name):
     name = subst(name, "VACATION", "VAC")
     return name
 
-def csv_to_dict (file):
-    """Reads in an org-mode table exported to csv, convert it a python dictionary"""
+
+def csv_to_dict(file):
+    """Reads in an org-mode table exported to csv, convert it a python
+    dictionary"""
 
     with open(file) as csvfile:
 
@@ -59,16 +63,17 @@ def csv_to_dict (file):
                 description = row[3]
                 hours = float(row[5])
 
-                if not project in projects:
-                    projects [project] = OrderedDict()
+                if project not in projects:
+                    projects[project] = OrderedDict()
 
-                if not day in projects[project]:
-                    projects [project][day] = {"hours" : 0.0, "description" : ""}
+                if day not in projects[project]:
+                    projects[project][day] = {"hours": 0.0, "description": ""}
 
-                projects [project][day]["hours"] += hours
-                projects [project][day]["description"] += description + "; "
+                projects[project][day]["hours"] += hours
+                projects[project][day]["description"] += description + "; "
 
         return projects
+
 
 def is_float(string):
     """Check if argument is a floating point number"""
@@ -78,18 +83,20 @@ def is_float(string):
     except ValueError:
         return False
 
-def check_project_active (name, time, dict):
-    """Check if a project had activity on a given day"""
-    d = time
-    for i in range(7):
-        if (d.month in projects):
-            if (prj in projects[d.month]):
-                if (d.day in projects[d.month][prj]):
-                    if (projects[d.month][prj][d.day]["hours"] > 0):
-                        return True
-        d = d + ONEDAY # INCREMENT
+# def check_project_active (name, time, dict):
+#     """Check if a project had activity on a given day"""
+#     d = time
+#     for i in range(7):
+#         if (d.month in projects):
+#             if (prj in projects[d.month]):
+#                 if (d.day in projects[d.month][prj]):
+#                     if (projects[d.month][prj][d.day]["hours"] > 0):
+#                         return True
+#         d = d + ONEDAY  # INCREMENT
+
 
 def project_to_summary(projects):
+    ""
 
     summaries = OrderedDict()
 
@@ -106,16 +113,16 @@ def project_to_summary(projects):
                     description = projects[year][month][prj][day]["description"] + "; "
 
                     # intialize the dict
-                    if not year in summaries:
-                        summaries [year] = OrderedDict()
-                    if not week in summaries[year]:
-                        summaries [year][week] = OrderedDict()
-                    if not prj in summaries[year][week]:
-                        summaries [year][week][prj] = OrderedDict()
-                    if not weekday in summaries[year][week][prj]:
-                        summaries [year][week][prj][weekday] = 0
-                    if not "notes" in summaries[year][week][prj]:
-                        summaries [year][week][prj]["notes"] = ""
+                    if year not in summaries:
+                        summaries[year] = OrderedDict()
+                    if week not in summaries[year]:
+                        summaries[year][week] = OrderedDict()
+                    if prj not in summaries[year][week]:
+                        summaries[year][week][prj] = OrderedDict()
+                    if weekday not in summaries[year][week][prj]:
+                        summaries[year][week][prj][weekday] = 0
+                    if "notes" not in summaries[year][week][prj]:
+                        summaries[year][week][prj]["notes"] = ""
 
                     summaries[year][week][prj][weekday] += hours
                     summaries[year][week][prj]["notes"] += description
@@ -126,17 +133,17 @@ def project_to_summary(projects):
                 notes = summaries[year][week][prj]["notes"]
 
                 # conditioning of the text
-                notes = re.sub(r"\s+"," ",notes) # REMOVE DUPLICATE SPACES
-                notes = re.sub(r",", ";",notes)  # CONVERT COMMAS TO SEMICOLONS
-                notes = re.sub(r"; ", ";",notes) # REMOVE SPACE SEPARATORS
+                notes = re.sub(r"\s+", " ", notes)  # REMOVE DUPLICATE SPACES
+                notes = re.sub(r", ", ";", notes)   # CONVERT COMMAS TO SEMICOLONS
+                notes = re.sub(r"; ", ";", notes)  # REMOVE SPACE SEPARATORS
 
                 notes = re.split(";", notes)
                 notes.sort()
-                notes = list(set(notes)) # REMOVE DUPLICATES
+                notes = list(set(notes))  # REMOVE DUPLICATES
                 notes.sort()
-                notes = ' '.join([str(elem)+";" for elem in notes]) # CONVERT FROM LIST TO STRING
-                notes = re.sub(r"^;\s*","",notes) # REMOVE THE FIRST SEMICOLON
-                notes = re.sub(r";\s*$","",notes) # REMOVE THE LAST SEMICOLON
+                notes = ' '.join([str(elem)+";" for elem in notes])  # CONVERT FROM LIST TO STRING
+                notes = re.sub(r"^;\s*", "", notes)  # REMOVE THE FIRST SEMICOLON
+                notes = re.sub(r";\s*$", "", notes)  # REMOVE THE LAST SEMICOLON
                 summaries[year][week][prj]["notes"] = notes
 
     return summaries
@@ -144,11 +151,11 @@ def project_to_summary(projects):
 
 def print_org_summary(summaries):
 
-    def print_spacer ():
+    def print_spacer():
         spacer = "|----------------------+-------+-------+-------+-------+-------+-------+-------+-------|"
         print(spacer)
 
-    def print_weekdays (time):
+    def print_weekdays(time):
         """"""
         d = time
         # print the day number headings
@@ -156,12 +163,12 @@ def print_org_summary(summaries):
         for weekday in range(7):
             if (d.day < 7 and d.weekday() > weekday):
                 diff = d.weekday() - weekday
-                princ ("% 6d*|" % (d-diff*ONEDAY).day )
+                princ("% 6d*|" % (d-diff*ONEDAY).day)
             else:
-                if (d.month==month):
-                    princ ("% 6d |" % d.day)
+                if d.month == month:
+                    princ("% 6d |" % d.day)
                 else:
-                    princ ("% 6d*|" % d.day)
+                    princ("% 6d*|" % d.day)
                 d = d + ONEDAY
         princ("       |\n")
 
@@ -169,68 +176,68 @@ def print_org_summary(summaries):
 
     for file in get_csv_files():
 
-         d = datetime.datetime.strptime(file.replace(".csv",""), "%Y-%m")
+        d = datetime.datetime.strptime(file.replace(".csv", ""), "%Y-%m")
 
-         month = d.month
-         year = d.year
+        month = d.month
+        year = d.year
 
-         d = datetime.datetime(year,month,1)
+        d = datetime.datetime(year, month, 1)
 
-         print("")
-         print("* %4d-%02d" % (year, month))
-         print("#+TBLNAME: %4d-%02d-Summary" % (year, month))
-         print("| %20s |    Mo |    Tu |    We |    Th |    Fr |    Sa |    Su | Notes |" %
-               ("%04d-%02d" % (year, month) ))
-         print_spacer()
+        print("")
+        print("* %4d-%02d" % (year, month))
+        print("#+TBLNAME: %4d-%02d-Summary" % (year, month))
+        print("| %20s |    Mo |    Tu |    We |    Th |    Fr |    Sa |    Su | Notes |" %
+              ("%04d-%02d" % (year, month)))
+        print_spacer()
 
-         while (d.month == month):
+        while d.month == month:
 
-            if (d.day==1 or d.weekday() == 0):
+            if (d.day == 1 or d.weekday() == 0):
                 print_weekdays(d)
                 print_spacer()
                 week = d.isocalendar()[1]
 
-                if (week in summaries[year]):
+                if week in summaries[year]:
                     for prj in summaries[year][week]:
-                        if (prj == "--"):
+                        if prj == "--":
                             continue
-                        princ ("| %20s |" % prj)
+                        princ("| %20s |" % prj)
                         for weekday in range(7):
                             if weekday in summaries[year][week][prj]:
-                                princ (" % 3.2f |" % summaries[year][week][prj][weekday])
+                                princ(" % 3.2f |" % summaries[year][week][prj][weekday])
                             else:
-                                princ ("     0 |")
+                                princ("     0 |")
                         princ("%s | \n" % summaries[year][week][prj]["notes"])
 
-            if (d.weekday()==6):
+            if d.weekday() == 6:
                 print_spacer()
 
             d = d + ONEDAY # INCREMENT
-         print_spacer()
+        print_spacer()
+
 
 def parse_projects():
     """Reads in Org exported CSV files and converts it into a
     dictionary of the form
     projects[year][month][project][day][hours/description]"""
 
-    projects=OrderedDict()
+    projects = OrderedDict()
 
     for file in get_csv_files():
-        #print("Parsing %s" % file)
-        d = datetime.datetime.strptime(file.replace(".csv",""), "%Y-%m")
+        d = datetime.datetime.strptime(file.replace(".csv", ""), "%Y-%m")
         month = d.month
         year = d.year
 
-        if (not year in projects):
-            projects[year]=OrderedDict()
+        if (year not in projects):
+            projects[year] = OrderedDict()
 
-        projects[year][month] = csv_to_dict (file)
+        projects[year][month] = csv_to_dict(file)
 
     return projects
 
 
 if __name__ == "__main__":
-    if sys.version_info < (3,0,0):
+    if sys.version_info < (3, 0, 0):
         print(__file__ + ' requires Python 3, while Python ' +
               str(sys.version[0] + ' was detected. Terminating. '))
         sys.exit(1)
